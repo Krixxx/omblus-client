c8bc94d1a1735638f433139b5ae785da4beef286
Kristjan <krix@planet.ee>
2022-01-30 18:28:19 +0200

    redux-toolkit initial setup. Set up store and slices

---
+++ src/app/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import authReducer from './slices/authSlice';
+import workersReducer from './slices/workersSlice';
+import filterReducer from './slices/filterSlice';
+
+export default configureStore({
+  reducer: {
+    auth: authReducer,
+    workers: workersReducer,
+    filter: filterReducer,
+  },
+});
