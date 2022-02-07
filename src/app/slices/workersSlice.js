82d01623284cb5d46c30f5e0687973baf54fcf83
Kristjan <krix@planet.ee>
2022-02-07 21:30:08 +0200

    initial Workers Slice. Livedata is missing

--- src/app/slices/workersSlice.js
+++ src/app/slices/workersSlice.js
@@ -1,11 +1,42 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import axios from "axios"
 
-const initialState = [];
+const initialState = {
+  workers: [],
+  status: "idle",
+  error: null,
+}
 
 const workersSlice = createSlice({
-  name: 'workers',
+  name: "workers",
   initialState,
   reducers: {},
-});
+  extraReducers(builder) {
+    builder
+      .addCase(fetchWorkersList.pending, (state, action) => {
+        state.status = "loading"
+      })
+      .addCase(fetchWorkersList.fulfilled, (state, action) => {
+        state.status = "succeeded"
+        //Add fetched information to the workers array
+        state.workers = state.workers.concat(action.payload)
+      })
+      .addCase(fetchWorkersList.rejected, (state, action) => {
+        state.status = "failed"
+        state.error = action.error.message
+      })
+  },
+})
 
-export default workersSlice.reducer;
+export const selectAllWorkersList = (state) => state.workers.workers
+
+export const fetchWorkersList = createAsyncThunk(
+  "workers/fetchWorkers",
+  async () => {
+    const response = await axios.get(process.env.REACT_APP_API_URL + `/workers`)
+
+    return response.data
+  }
+)
+
+export default workersSlice.reducer
