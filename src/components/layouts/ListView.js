6a55728ec8b8b3b34a45a5fa72d33c19cbe93bf7
Kristjan <krix@planet.ee>
2022-02-19 12:48:31 +0200

    ListView layout done

--- src/components/layouts/ListView.js
+++ src/components/layouts/ListView.js
@@ -1,7 +1,36 @@
 import React from "react"
+import styled from "styled-components"
 
-const ListView = () => {
-  return <div>ListView</div>
+import Container from "@mui/material/Container"
+import Stack from "@mui/material/Stack"
+
+import { ListItem } from "../../components"
+
+const ListView = ({ workers }) => {
+  return (
+    <Container component="main" maxWidth="lg">
+      <Wrapper>
+        <Stack alignItems="center" spacing={2}>
+          <table className="workers">
+            <tbody>
+              {workers.map((worker) => {
+                return <ListItem key={worker.id} {...worker} />
+              })}
+            </tbody>
+          </table>
+        </Stack>
+      </Wrapper>
+    </Container>
+  )
 }
+
+const Wrapper = styled.div`
+  padding-top: 2rem;
+  .workers {
+    margin-top: 12px;
+    width: 80vw;
+    max-width: 700px;
+  }
+`
 
 export default ListView
