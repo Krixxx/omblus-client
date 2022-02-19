b1d1a57e87602e1d133af5a9ad4a7e3c0d8da171
Kristjan <krix@planet.ee>
2022-02-19 12:06:37 +0200

    GridItem finished

--- src/components/layouts/GridView.js
+++ src/components/layouts/GridView.js
@@ -1,7 +1,31 @@
 import React from "react"
+import styled from "styled-components"
 
-const GridView = () => {
-  return <div>GridView</div>
+import Container from "@mui/material/Container"
+import Grid from "@mui/material/Grid"
+
+import { GridItem } from "../../components"
+
+const GridView = ({ workers }) => {
+  return (
+    <Container component="main" maxWidth="xl">
+      <Wrapper>
+        <Grid container justifyContent="center" spacing={2}>
+          {workers.map((worker) => {
+            return (
+              <Grid item key={worker.id}>
+                <GridItem {...worker} />
+              </Grid>
+            )
+          })}
+        </Grid>
+      </Wrapper>
+    </Container>
+  )
 }
+
+const Wrapper = styled.div`
+  padding-top: 2rem;
+`
 
 export default GridView
