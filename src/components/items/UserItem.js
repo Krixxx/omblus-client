37654088e44d0fb8c1b4dbe10091b57bf3a2b8e4
Kristjan <krix@planet.ee>
2022-02-25 20:01:44 +0200

    TableRow set up

--- src/components/items/UserItem.js
+++ src/components/items/UserItem.js
@@ -1,7 +1,26 @@
 import React from "react"
 
+import DeleteIcon from "@mui/icons-material/Delete"
+import TableRow from "@mui/material/TableRow"
+import TableCell from "@mui/material/TableCell"
+import Button from "@mui/material/Button"
+
 const UserItem = ({ user }) => {
-  return <div>{user.username}</div>
+  const handleDelete = (id) => {
+    console.log(id)
+  }
+
+  return (
+    <TableRow>
+      <TableCell>{user.username}</TableCell>
+      <TableCell align="right">{user.role}</TableCell>
+      <TableCell align="right">
+        <Button onClick={() => handleDelete(user.id)}>
+          <DeleteIcon />
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
 }
 
 export default UserItem
